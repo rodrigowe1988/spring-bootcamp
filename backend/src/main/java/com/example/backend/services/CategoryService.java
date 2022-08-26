45ea0d36c8dc5eeb4cda1ea53fa9e82575a3a592
Rodrigo Weber <rodrigoweber@pop-os.localdomain>
2022-08-26 07:58:33 -0300

    h2 database

---
+++ backend/src/main/java/com/example/backend/services/CategoryService.java
@@ -0,0 +1,19 @@
+package com.example.backend.services;
+
+import com.example.backend.entities.Category;
+import com.example.backend.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+}
